a55a1dec91da662f12f12817dfb1e2d662793497
Arisha-Akbar <arishaakbar713@gmail.com>
2026-01-17 00:04:03 +0500

    added mock data

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-const express =  require("express");
+const express = require("express");
+const users = require("./MOCK_DATA.json");
 const app = express();
+const PORT = 8000;
+
+app.get("/api/users", (req, res) => {
+  return res.json(users);
+});
+
+app.get("/users", (req, res) => {
+  const html = ` 
+   <ul>
+        ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
+   
+   </ul>
+   `;
+  return res.send(html);
+});
+app.listen(8000, () => console.log(`Server started at port: ${PORT}`));
